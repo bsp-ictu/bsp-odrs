4117d587e281cc2d5e8ca8cde729933c243c42d4
bsp-ictu <ictu@scouts.gov.ph>
2026-06-18 13:41:28 +0800

    Reorganize database connection parameters for clarity

--- Login/Database/process-configuration.php
+++ Login/Database/process-configuration.php
@@ -1,10 +1,10 @@
 <?php
-    $serverName = "localhost\SQLEXPRESS";
-    $database = "bsp_document_request";
-    $username = "bsp_php";
-    $password = "BSP1234";
-
     try {
+        $serverName = "localhost\SQLEXPRESS";
+        $database = "bsp_document_request";
+        $username = "bsp_php";
+        $password = "BSP1234";
+        
         $conn = new PDO("sqlsrv:Server=$serverName;Database=$database", $username, $password);
         // Set error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
